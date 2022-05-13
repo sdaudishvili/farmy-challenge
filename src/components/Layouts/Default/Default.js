a0a67f1392147123744361e13a88607753e2e8b5
Sandro Daudishvili <sdaudishvili@gmail.com>
2022-05-13 18:55:25 +0400

    refactor: header

--- src/components/Layouts/Default/Default.js
+++ src/components/Layouts/Default/Default.js
@@ -13,10 +13,6 @@ const DefaultLayout = () => {
           <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
       <Container sx={{ marginTop: 3 }}>
--- src/views/Salad/Create/Create.js
+++ src/views/Salad/Create/Create.js
@@ -63,7 +63,6 @@ const CreateSalad = () => {
               numOfServings: x.numOfServings
             }))
           });
-          console.log(item);
         } catch (error) {
           console.log(error);
         }
